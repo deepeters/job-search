16422ec2c3a529c0ddb7ac52ec6ead3c27b8dd79
Dennis Peters <dennis95peters@gmail.com>
2021-05-20 18:29:50 +0300

    THis commit builds the first even for the button

--- app/src/main/java/com/example/jobsearch/MainActivity.java
+++ app/src/main/java/com/example/jobsearch/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.jobsearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mgetStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mgetStartedButton = (Button)findViewById(R.id.getStartedButton);
+            mgetStartedButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
