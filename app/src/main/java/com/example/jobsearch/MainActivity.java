0c0c1f7e46993fdcf9e4d85bae9a7a9c1d44b5d6
Dennis Peters <dennis95peters@gmail.com>
2021-05-21 07:59:20 +0300

    This commit adds the second activity

--- app/src/main/java/com/example/jobsearch/MainActivity.java
+++ app/src/main/java/com/example/jobsearch/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jobsearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
             mgetStartedButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, GeneralActivity.class);
+                    startActivity(intent);
                 }
             });
     }
